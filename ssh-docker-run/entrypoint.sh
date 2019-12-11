cfba91af6d8d5bf137f7765419e7fe1df62196e7
Arkadiy Ekimov <arkadiy.ekimov@natlex.ru>
2019-12-11 17:32:44 +0300

    10t

--- ssh-docker-run/entrypoint.sh
+++ ssh-docker-run/entrypoint.sh
@@ -25,7 +25,7 @@ function main() {
 
   #ssh -o StrictHostKeyChecking=no -A -tt $INPUT_USER@$INPUT_HOST "$HOME/run.sh $INPUT_IAM"
   ls -a
-  scp -o StrictHostKeyChecking=no $HOME/ssh-docker-run/run.sh $INPUT_USER@$INPUT_HOST:/home/$INPUT_USER/
+  scp -o StrictHostKeyChecking=no ./ssh-docker-run/run.sh $INPUT_USER@$INPUT_HOST:/home/$INPUT_USER/
   ssh -o StrictHostKeyChecking=no -A -tt $INPUT_USER@$INPUT_HOST "$HOME/run.sh $INPUT_IAM"
   #ssh -o StrictHostKeyChecking=no -A -tt $INPUT_USER@$INPUT_HOST "mkdir test-github"
 
