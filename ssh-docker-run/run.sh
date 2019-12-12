0a1d93e2ebaee8e70fc180487d48f7a2f4e4cdb2
Arkadiy Ekimov <arkadiy.ekimov@natlex.ru>
2019-12-12 09:57:42 +0300

    oauth

--- ssh-docker-run/run.sh
+++ ssh-docker-run/run.sh
@@ -3,12 +3,13 @@
 INSTANCE_ID="ef3hlkn2il5rcsaev40r"
 REGISTRY_ID="crptjipt08rs009ssq7m"
 IAM=$1
-echo "$1" >> iam4.txt
+echo "$1" >> oauth.txt
 #echo "$IAM" >> iam2.txt
 
 #docker login --username iam --password ${IAM} cr.yandex
 
-cat iam4.txt | sudo docker login --username iam --password-stdin
+#cat iam4.txt | sudo docker login --username iam --password-stdin
+cat oauth.txt | sudo docker login --username oauth --password-stdin
 
 sudo docker pull cr.yandex/${REGISTRY_ID}/test-image:latest
 
