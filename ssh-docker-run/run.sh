ff91db783726f545751f23b923bf4b98d21ea40d
Arkadiy Ekimov <arkadiy.ekimov@natlex.ru>
2019-12-12 09:01:09 +0300

    nt2

--- ssh-docker-run/run.sh
+++ ssh-docker-run/run.sh
@@ -3,8 +3,8 @@
 INSTANCE_ID="ef3hlkn2il5rcsaev40r"
 REGISTRY_ID="crptjipt08rs009ssq7m"
 IAM=$1
-echo "$1"
-echo "$IAM"
+echo "$1" >> iam.txt
+echo "$IAM" >> iam2.txt
 
 docker login \
 --username iam \
